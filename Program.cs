918f8704c37f470dc7383a00a86f0682f60beb40
venu sripada <avenusripada@gmail.com>
2021-03-08 17:49:56 -0600

    initial push

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+
+namespace ad_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             using (var context = new PrincipalContext(ContextType.Domain, "domain.net", "serviceaccount user", "service account pwd"))
+                {
+                   
+                        String userName = "username";
+                        String password = "password";
+                   
+
+                    if (context.ValidateCredentials(userName, password))
+                    {
+                         Console.WriteLine("User exists in AD " + userName);
+                         Console.WriteLine("Updating user in Okta: " + userName);
+                    }
+                       else
+                    {
+                         Console.WriteLine("Cannot authenticate user in AD: " + userName);
+                    }
+
+        }
+    }
+}
+}
